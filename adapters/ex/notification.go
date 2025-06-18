719a3653d7c82da9a22aa35f80a9f1a42d9c110e
QUDUSKUNLE <quduskunle@gmail.com>
2025-06-18 03:35:44 +0100

    Delete email.go

--- adapters/ex/notification.go
+++ adapters/ex/notification.go
@@ -4,6 +4,9 @@ import (
 	"os"
 
 	"github.com/sendgrid/sendgrid-go"
+	"fmt"
+
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
 // SendGridEmailService implements EmailService using SendGrid
@@ -37,4 +40,22 @@ func NewGmailConfig(c GmailConfig) *GmailConfig {
 		Password: c.Password,
 		From:     c.From,
 	}
+}
+
+// SendEmail sends an email using SendGrid
+func (s *SendGridEmailService) SendEmail(to string, subject string, body string) error {
+	from := mail.NewEmail("Medicue", s.from)
+	recipient := mail.NewEmail("", to)
+	message := mail.NewSingleEmail(from, subject, recipient, "", body)
+
+	response, err := s.client.Send(message)
+	if err != nil {
+		return fmt.Errorf("failed to send email: %w", err)
+	}
+
+	if response.StatusCode >= 400 {
+		return fmt.Errorf("email service returned error status: %d", response.StatusCode)
+	}
+
+	return nil
 }
