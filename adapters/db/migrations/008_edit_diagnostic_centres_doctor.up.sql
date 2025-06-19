c5e78812724ec7a176239bc7897145584ef78b27
QUDUSKUNLE <quduskunle@gmail.com>
2025-06-19 20:09:00 +0100

    Delete file migration

--- adapters/db/migrations/008_edit_diagnostic_centres_doctor.up.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE diagnostic_centres
-ALTER COLUMN doctors TYPE TEXT[] USING doctors::TEXT[],
-ALTER COLUMN available_tests TYPE TEXT[] USING available_tests::TEXT[],
-ALTER COLUMN doctors SET DEFAULT ARRAY['Male']::TEXT[],
-ALTER COLUMN available_tests SET DEFAULT ARRAY['CT_SCAN']::TEXT[];
-
