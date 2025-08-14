91de0b3298dbdacceb245154c3385ac67e53ed03
QUDUSKUNLE <quduskunle@gmail.com>
2025-08-14 17:29:27 +0100

    Reset host

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ import (
 // @title Diagnoxix
 // @version 1.0
 // @description Diagnoxix API
-// @host 127.0.0.1:7556
+// @host diagnoxix.onrender.com/
 // @BasePath /
 func main() {
 	// Initialize logger with custom configuration
