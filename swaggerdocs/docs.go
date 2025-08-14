b1ff9a32f4a0609dc318fe001afb044144048e9e
QUDUSKUNLE <quduskunle@gmail.com>
2025-08-14 20:52:18 +0100

    Change back slash

--- swaggerdocs/docs.go
+++ swaggerdocs/docs.go
@@ -4973,7 +4973,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "diagnoxix.onrender.com/",
+	Host:             "diagnoxix.onrender.com",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Diagnoxix",
